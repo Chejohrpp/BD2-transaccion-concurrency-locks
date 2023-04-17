754e1604eb633191769fc2608d4b69b0293acbcb
Chejohrpp <sergioroherp@gmail.com>
2023-04-17 01:22:58 -0600

    se cambio la accesibilidad para la base de datos

--- connection.py
+++ connection.py
@@ -1,10 +1,23 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
 # Configuración de la conexión a la base de datos
+
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+host = os.getenv('HOST')
+database = os.getenv('DATABASE')
+
+# Acceder a las variables de entorno
 config = {
-    "user": "hrp",
-    "password": "1234",
-    "host": "localhost",
-    "database": "mi_base_datos"
+    "user": user,
+    "password": password,
+    "host": host,
+    "database": database
 }
 
 def new_connection():
